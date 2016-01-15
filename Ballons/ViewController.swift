44349e5bb04e0407ef18c0b958f1f1e5d6b9451f
Alan <alancf@bellsouth.net>
2016-01-15 12:53:47 -0500

    working

--- Ballons/ViewController.swift
+++ Ballons/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redBalloon: UIImageView!;
+    @IBOutlet weak var blueBalloon: UIImageView!;
+    @IBOutlet weak var blueBtn: UIButton!;
+    @IBOutlet weak var redBtn: UIButton!;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func redClick(sender: AnyObject) {
+        redBalloon.hidden = true;
+        redBtn.hidden = true;
+        blueBtn.hidden = false;
+        blueBalloon.hidden = false;
+    }
 
+    @IBAction func blueClick(sender: AnyObject) {
+        blueBalloon.hidden = true;
+        blueBtn.hidden = true;
+        redBalloon.hidden = false;
+        redBtn.hidden = false;
+    }
 }
 
